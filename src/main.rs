fbf06ac2b94c392af04d2ce93ebc8b4b1532c705
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2020-09-12 20:54:32 -0500

    Add some OOP

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,117 @@ fn welcome(){
     println!("Write down the expression to solve or type 0 to exit");
 }
 
+fn has_operations(strexp: String) -> bool {
+    if strexp.contains("+") || strexp.contains("-") || strexp.contains("*") || strexp.contains("/") || strexp.contains("(") || strexp.contains(")"){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+enum OperationType{
+    Add,
+    Substract,
+    Multiply,
+    Divide,
+}
+
+struct Operation {
+    first: String,
+    operation: OperationType,
+    second: String,
+}
+
+impl Operation {
+    fn new(&self, first: String, operation: OperationType, second: String) -> Operation {
+        Operation {
+            first:first,
+            operation:operation,
+            second:second,
+        }
+    }
+    fn get_result(&self) -> i64{
+        // Get numbers
+        let mut first_num: i64 = 0;
+        let mut second_num: i64 = 0;
+
+        let first_str = self.first.clone();
+        let second_str = self.second.clone();
+
+        if has_operations(first_str){
+            let oper1 = Operation{
+                first:"".to_string(),
+                operation: OperationType::Add,
+                second:"".to_string(),
+            };
+            Operation::parse(&oper1, self.first.clone());
+            first_num = Operation::get_result(&oper1);
+        }else{
+            first_num = self.first.parse::<i64>().unwrap();
+        }
+
+        if has_operations(second_str){
+            let oper2 = Operation{
+                first:"".to_string(),
+                operation: OperationType::Add,
+                second:"".to_string(),
+            };
+            Operation::parse(&oper2, self.second.clone());
+            second_num = Operation::get_result(&oper2);
+        }else{
+            second_num = self.second.parse::<i64>().unwrap();
+        }
+
+        // Operate
+        match self.operation {
+            OperationType::Add => first_num + second_num,
+            OperationType::Substract => first_num - second_num,
+            OperationType::Multiply => first_num * second_num,
+            OperationType::Divide => first_num / second_num
+        }
+    }
+
+    fn parse(&self, strexp: String) -> Operation {
+        // Operation Ordering
+        if strexp.contains("/"){
+            let index = strexp.find("/");
+            // TODO: Remove this
+            return Operation{
+                first:strexp,
+                operation: OperationType::Add,
+                second:"0".to_string()
+            }
+        }else if strexp.contains("*") {
+            // TODO: Remove this
+            return Operation{
+                first:strexp,
+                operation: OperationType::Add,
+                second:"0".to_string()
+            }
+        }else if strexp.contains("-"){
+            // TODO: Remove this
+            return Operation{
+                first:strexp,
+                operation: OperationType::Add,
+                second:"0".to_string()
+            }
+        }else if strexp.contains("+"){
+            // TODO: Remove this
+            return Operation{
+                first:strexp,
+                operation: OperationType::Add,
+                second:"0".to_string()
+            }
+        }else{
+            return Operation{
+                first:strexp,
+                operation: OperationType::Add,
+                second:"0".to_string()
+            }
+        }
+    }
+}
+
 fn remove_spaces(strexp: String) -> String {
     return strexp.replace(" ", "").replace("\n","");
 }
@@ -18,19 +129,7 @@ fn parentheses(strexp: String) -> String {
         .replace("}",")");
 }
 
-fn define_oper_order(expr: String) -> String {
-    // Order operations by operation order without including what is inside parethesis
-        // Remove Parentheses and change by letters
-        // Create an array of characters from the string
-        // Get the index of every operator
-        // Order every index according to the operation order
-        // Divide the indexes in groups according to the operation related
-        // Surround with parentheses the first index
-        // Repeat all the steps
-    // Create parentheses around first type operation
-    // Order operations by operation order without including what is inside parethesis
-    // Create parentheses around second type operation
-    // Repeat until ending
+fn to_operations(expr: String) -> String {
     return expr;
 }
 fn main() {
@@ -52,7 +151,7 @@ fn main() {
 
         expr = remove_spaces(expr);
         expr = parentheses(expr);
-        expr = define_oper_order(expr);
+        expr = to_operations(expr);
         println!("{}", expr);
         
         // Clean variable data
